07073043b803f6da623eded1b5910688d4a9eb88
Prometheus <Prometheus@DESKTOP-VI5LP0C>
2026-07-17 10:01:44 -0700

    Added missing back button to Exclusive Items page.

--- SimToolsApp/Pages/ExclusiveItems.xaml.cs
+++ SimToolsApp/Pages/ExclusiveItems.xaml.cs
@@ -117,10 +117,7 @@ namespace SimTools
 
         }
 
-        private void BackButton_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
+        private void BackButton_Click(object sender, RoutedEventArgs e) => Close();
 
         private string GetArchivePassword()
         {
